c733d6f7de8da56efe406999ecd452e574571582
RC <recomi40@gmail.com>
2018-04-12 23:52:55 -0400

    added Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## Liri Node App
+
+* Description: LIRI is a language interpretation recognition application that reads in a command and argument. The commands will allow
+users to return movie details, tweets, and song details. 
+
+
+## Notes
+
+* Users will need to provide their own twitter and spotify API keys to get started. 
--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
+// variables to run node packages
 var fs = require("fs");
 var request = require("request");
-var keys = require("./keys.js");
+var keys = require("./keys.js");//links api keys file
 var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
 var Spotify = require('node-spotify-api');
@@ -10,6 +11,8 @@ var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var value = "";
 
+
+//reads the input from user and routes
 switch (command) {
     case "my-tweets":
         getTweets();
@@ -24,9 +27,12 @@ switch (command) {
         getMovie(omdbApi, value);
         break;
     case "do-what-it-says":
-        doWhatItSays(); break;
+        doWhatItSays(); 
+        break;
 };
 
+//gets the total users imput after the command
+// movie title, song name and returns the value
 function getArguments(process) {
     for (var i = 3; i < process.length; i++) {
         if (i > 3 && i < process.length) {
@@ -39,9 +45,10 @@ function getArguments(process) {
     return value;
 }
 
+//returns the last 20 tweets my-tweets
 function getTweets() {
     client.get("statuses/user_timeline/", function (error, tweets, response) {
-        for (var i = 0; i < tweets.length; i++) {
+        for (var i = 0; i < 20; i++) {
             var twitterResults =
                 "\r\n@" + tweets[i].user.screen_name + " - " +
                 tweets[i].text + "\r\n" +
@@ -51,10 +58,14 @@ function getTweets() {
     });
 }
 
+//returns the user entered song, if no song is entered it returns song
+//The Sign
 function getSpotifySong(songName) {
     if (!songName) {
         songName = "The Sign";
     }
+
+    //list of song information data returned from the Spotify api
     spotify.search({ type: 'track', query: songName }, function (err, data) {
         for (var i = 0; i < 1; i++) {
             var results =
@@ -68,10 +79,13 @@ function getSpotifySong(songName) {
     });
 };
 
+//returns the user entered movie , if no movie returns the movie
+//Mr Nobody
 function getMovie(omdbApi, movie) {
     if (!movie) {
         movie = "Mr. Nobody";
     }
+    //list of movie information data returned from OMDB api
     request(omdbApi + movie, function (error, response, body) {
         var movieObject = JSON.parse(body);
         var movieResults =
@@ -86,4 +100,39 @@ function getMovie(omdbApi, movie) {
             movieObject.Actors + "\r\n";
         console.log(movieResults);
     });
-};
+}
+
+//Read text from the random.txt and run the command 
+//based on what is in the file
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        data = data.split(',');
+
+        var fileCommand;
+        var fileParameter;
+
+        if (data.length == 2) {
+            fileCommand = data[0];
+            fileParameter = data[1];
+        }
+
+        fileParameter = fileParameter.replace('"', '');
+
+        switch (fileCommand) {
+            case 'my-tweets':
+                value = fileParameter;
+                getTweets();
+                break;
+
+            case 'spotify-this-song':
+                value = fileParameter;
+                getSpotifySong(value);
+                break;
+
+            case 'movie-this':
+                value = fileParameter;
+                getMovie();
+                break;
+        }
+    });
+}
